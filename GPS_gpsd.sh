491a8585af43241a98c2d4283d4afce47cce9f97
Hakan Demirel <hakan.demirel@student.kit.edu>
2014-12-08 03:55:23 +0100

    verbessert
    v2

--- GPS_gpsd.sh
+++ GPS_gpsd.sh
@@ -6,10 +6,13 @@ echo "GPS-Gerät wird geladen"
 #sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
 #sudo service ntp restart
 #sudo ntpd
-python /home/pi/gps/gpstime.py
-python /home/pi/gps/python/ordner.py
-echo "Ordner erstelltn"
-python /home/pi/gps/python/gpsdData.py
+sudo python /home/pi/gps/gpstime.py
+echo "Zeitsychronisiert"
+sudo python /home/pi/gps/python/ordner.py
+echo "Ordner erstellt"
+sudo gpxlogger -d -f /home/pi/gps/output/`date +"%Y-%m-%d"`/GPX_`date +"%Y.%m.%d_%H:%M:%S"`.gpx -m 50
+echo "GPX-Daten geschrieben"
+sudo python /home/pi/gps/python/gpsdData.py
 echo "GPS-Daten geschrieben"
 after=$(date +%s)
 echo "elapsed time:" $((after - $before)) "seconds"
